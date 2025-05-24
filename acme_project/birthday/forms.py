791807c762a3c91d174f01c66286a5182fba7ebf
Evgeniy Zhuravlev <ptichkinjj@gmail.com>
2025-05-25 00:16:00 +0300

    add CBV etc

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Birthday
+
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
+
+
+class BirthdayForm(forms.ModelForm):
+    class Meta:
+        model = Birthday
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        # Вызов родительского метода clean.
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
